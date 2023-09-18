450a28e789a6eec6a1f3a64460d465ee669726fd
George Gordon <ggordon2019@fau.edu>
2023-09-17 22:05:20 -0400

    Step 9 added album images to a viewable album tab and followed justin and alex's instructions to fix connection to view controller and add AlbumCell the inspector

--- lab-tunley/AlbumsViewController.swift
+++ lab-tunley/AlbumsViewController.swift
@@ -6,14 +6,39 @@
 //
 
 import UIKit
+import Nuke
 
-class AlbumsViewController: UIViewController {
+class AlbumsViewController: UIViewController, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        albums.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        // Get a collection view cell (based in the identifier you set in storyboard) and cast it to our custom AlbumCell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "AlbumCell", for: indexPath) as! AlbumCell
 
+        // Use the indexPath.item to index into the albums array to get the corresponding album
+        let album = albums[indexPath.item]
+
+        // Get the artwork image url
+        let imageUrl = album.artworkUrl100
+
+        // Set the image on the image view of the cell
+        Nuke.loadImage(with: imageUrl, into: cell.albumImageView)
+
+        return cell
+    }
+    
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     var albums: [Album] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        collectionView.dataSource = self
+        
         // Do any additional setup after loading the view.
         // Create a search URL for fetching albums (`entity=album`)
         let url = URL(string: "https://itunes.apple.com/search?term=blackpink&attribute=artistTerm&entity=album&media=music")!
@@ -38,8 +63,11 @@ class AlbumsViewController: UIViewController {
                 let response = try decoder.decode(AlbumSearchResponse.self, from: data)
                 let albums = response.results
                 
-                self?.albums = albums
-                print(albums)
+                DispatchQueue.main.async {
+                    self?.albums = albums
+                    self?.collectionView.reloadData()
+                }
+                //print(albums)
             } catch {
                 print(error.localizedDescription)
             }
