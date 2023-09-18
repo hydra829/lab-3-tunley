d082024073ccc910f8ffdcd8534442d84ad36455
George Gordon <ggordon2019@fau.edu>
2023-09-17 20:19:21 -0400

    Completed Step 6

--- lab-tunley/AlbumsViewController.swift
+++ lab-tunley/AlbumsViewController.swift
@@ -9,10 +9,44 @@ import UIKit
 
 class AlbumsViewController: UIViewController {
 
+    var albums: [Album] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        // Create a search URL for fetching albums (`entity=album`)
+        let url = URL(string: "https://itunes.apple.com/search?term=blackpink&attribute=artistTerm&entity=album&media=music")!
+        let request = URLRequest(url: url)
+
+        let task = URLSession.shared.dataTask(with: request) { [weak self] data, response, error in
+
+            // Handle any errors
+            if let error = error {
+                print("❌ Network error: \(error.localizedDescription)")
+            }
+
+            // Make sure we have data
+            guard let data = data else {
+                print("❌ Data is nil")
+                return
+            }
+
+            let decoder = JSONDecoder()
+            do {
+                // Try to parse the response into our custom model
+                let response = try decoder.decode(AlbumSearchResponse.self, from: data)
+                let albums = response.results
+                
+                self?.albums = albums
+                print(albums)
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+
+        // Initiate the network request
+        task.resume()
     }
     
 
---
+++ lab-tunley/Models/Album.swift
@@ -0,0 +1,16 @@
+//
+//  Album.swift
+//  lab-tunley
+//
+//  Created by George Gordon on 9/17/23.
+//
+
+import Foundation
+
+struct AlbumSearchResponse: Decodable {
+    let results: [Album]
+}
+
+struct Album: Decodable {
+    let artworkUrl100: URL
+}
